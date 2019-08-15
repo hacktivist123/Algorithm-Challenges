aa9d9b9c2319e58a75d32a23d1b306218a0c9979
Shedrack akintayo <akintayoshedrack@gmail.com>
2019-08-15 16:38:37 +0100

    Update convert-celsius-to-fahrenheit.js

--- Basic Algorithms/convert-celsius-to-fahrenheit.js
+++ Basic Algorithms/convert-celsius-to-fahrenheit.js
@@ -1,18 +1,14 @@
-//The algorithm to convert from Celsius to Fahrenheit is the temperature
+/* The algorithm to convert from Celsius to Fahrenheit is the temperature
 in Celsius times 9/5, plus 32.
-
-//You are given a variable celsius representing a temperature in Celsius.
+You are given a variable celsius representing a temperature in Celsius.
 Use the variable fahrenheit already defined and apply the algorithm to assign
-it the corresponding temperature in Fahrenheit.
+it the corresponding temperature in Fahrenheit. */
 
 function convertToF(celsius) {
 
   var fahrenheit;
-  // Only change code below this line
 
   fahrenheit = celsius * (9/5) + 32;
-
-  // Only change code above this line
   return fahrenheit;
 }
 
